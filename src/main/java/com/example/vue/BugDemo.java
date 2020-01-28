b99187beecf432fc20fde468b6d8015a1ef0861a
Lily Huang <945349588@qq.com>
2020-01-28 16:02:56 +0800

    pmd检查示例

--- src/main/java/com/example/vue/BugDemo.java
+++ src/main/java/com/example/vue/BugDemo.java
@@ -4,10 +4,20 @@ public class BugDemo {
 
 	public static void main(String[] args) {
 		String s = null;
+		
+		int i = 0;
+
 		if (s.equals("0")) {
 			System.out.println("Find a bug.");
 		} else {
 			System.out.println("Another bug.");
+		}
+
+		int j = 0;
+		if (j == 0) {
+
+		} else {
+
 		}
 	}
 }
